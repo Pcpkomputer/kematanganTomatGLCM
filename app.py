3c8c74bc35a3d3000bc59345ddcb73f8b21794c7
18120573 <terrificsubs@gmail.com>
2021-05-05 23:41:09 +0700

    feat: ...

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/detection")
+def detection():
+    return render_template("detecttomato.html")
+
+@app.route("/logs")
+def logs():
+    return render_template("detectionlogs.html")
+
 if __name__=='__main__':
     app.run(debug=True)
