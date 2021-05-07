72f16a839ba35d7ab47a2db6f339442a3de2f679
18120573 <terrificsubs@gmail.com>
2021-05-07 22:10:37 +0700

    feat: ...

--- app.py
+++ app.py
@@ -3,6 +3,13 @@ import os
 import mysql.connector
 import psutil
 import time
+from utils.feature_extraction import calc_glcm_all_agls
+import cv2
+import numpy as np
+import uuid
+from PIL import Image
+import tensorflow
+from tensorflow import keras
 
 app = Flask(__name__)
 
@@ -29,9 +36,29 @@ def index():
 
     return render_template("index.html",rows=rows, cpuusage=psutil.cpu_percent(),memoryusage=psutil.virtual_memory()[2],requesttime=request_time)
 
-@app.route("/detection")
+@app.route("/detection", methods=["POST","GET"])
 def detection():
-    modelexist = os.path.exists(os.path.join(os.getcwd(),"model.joblib"))
+    if request.method=="POST" and request.files["file"]:
+        image = request.files["file"]
+        id = str(uuid.uuid1())
+        image.save("static/classification/"+id+".jpg")
+        # PROSES PREPROCESSING
+        image = cv2.imread("static/classification/"+id+".jpg")
+        #image = image[1408:1408+2208,0:0+2176]
+        gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+        resize = cv2.resize(gray, (300,500))
+
+        properties = ['dissimilarity', 'correlation', 'homogeneity', 'contrast', 'ASM', 'energy']
+        feature = calc_glcm_all_agls(resize, "undefined", props=properties)
+
+        model = keras.models.load_model("model.h5")
+        logits = model.predict([feature])
+        
+        print(tensorflow.nn.softmax(logits))
+
+        return "123"
+
+    modelexist = os.path.exists(os.path.join(os.getcwd(),"model.h5"))
     return render_template("detecttomato.html",modelexist=modelexist)
 
 @app.route("/logs")
@@ -48,8 +75,8 @@ def logs():
 @app.route("/model", methods=["POST","GET"])
 def model():
     if request.method=="POST" and request.files["file"]:
-        request.files["file"].save("model.joblib")
-        return "damn"
+        request.files["file"].save("model.h5")
+        return render_template("selectmodel.html",success="Model berhasil diupload...")
     return render_template("selectmodel.html")
 if __name__=='__main__':
     app.run(debug=True)
